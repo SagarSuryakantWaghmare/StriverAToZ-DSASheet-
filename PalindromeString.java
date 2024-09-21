ddacf4e852e0f17c33566278f36b61cfde838342
Sagar <>
2024-09-21 22:16:52 +0530

    Update PalindromeString.java

--- PalindromeString.java
+++ PalindromeString.java
@@ -1,6 +1,6 @@
 public class PalindromeString {
     public static boolean isPalindrome(String s){
-      String cleanString=s.replaceAll("[a-zA-Z0-9]", "").toLowerCase();  
+      String cleanString=s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();  
       int left=0;
       int right=cleanString.length()-1;
       while (left<right) {
