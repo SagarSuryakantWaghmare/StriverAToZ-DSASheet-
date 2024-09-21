a227a28d3c8ea247bee95d6ab43df8daa81738b2
Sagar <>
2024-09-21 22:10:40 +0530

    ValidPalindrome

---
+++ PalindromeString.java
@@ -0,0 +1,19 @@
+public class PalindromeString {
+    public static boolean isPalindrome(String s){
+      String cleanString=s.replaceAll("[a-zA-Z0-9]", "").toLowerCase();  
+      int left=0;
+      int right=cleanString.length()-1;
+      while (left<right) {
+        if(cleanString.charAt(left)!=cleanString.charAt(right)){
+            return false;
+        }
+        left++;
+        right--;
+      }
+      return true;
+    }
+   public static void main(String[] args) {
+    String s="A man, a plan, a canal: Panama";
+    System.out.println(isPalindrome(s));
+   } 
+}
