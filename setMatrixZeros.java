0b437aa3e41344fb17f63b56e2f309c2a6f252b0
Sagar <>
2024-10-10 11:46:36 +0530

    SetMatrixOptimalSolution

--- setMatrixZeros.java
+++ setMatrixZeros.java
@@ -40,30 +40,67 @@ public class setMatrixZeros {
 
     // Better solution:
 
-    public static void setZeroes(int[][]matrix){
+//     // public static void setZeroes(int[][]matrix){
 
-        int n=matrix.length;
-        int m=matrix[0].length;
-        // First make the two array 
-        int row[]=new int[n];
-        int col[]=new int[m];
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if(matrix[i][j]==0){
-                     row[i]=1;
-                     col[j]=1;
+//         int n=matrix.length;
+//         int m=matrix[0].length;
+//         // First make the two array 
+//         int row[]=new int[n];
+//         int col[]=new int[m];
+//         for (int i = 0; i < matrix.length; i++) {
+//             for (int j = 0; j < matrix[0].length; j++) {
+//                 if(matrix[i][j]==0){
+//                      row[i]=1;
+//                      col[j]=1;
+//                 }
+//             }
+//         }
+//         for (int i = 0; i < matrix.length; i++) {
+//             for (int j = 0; j < matrix[0].length; j++) {
+//                 if(row[i]==1||col[j]==1){
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
+//     }
+public static void setZeroes(int[][]matrix){
+    int n=matrix.length;
+    int m=matrix[0].length;
+    int col0=1;
+    for (int i = 0; i < n; i++) {
+           for (int j = 0; j < matrix[0].length; j++) {
+            if(matrix[i][j]==0){
+                matrix[i][0]=0;
+                if(j!=0){
+                        matrix[0][j]=0;
+                }
+                else{
+                    col0=0;
                 }
             }
-        }
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[0].length; j++) {
-                if(row[i]==1||col[j]==1){
-                    matrix[i][j]=0;
+           } 
+    }
+
+    for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if(matrix[i][j]!=0){
+                    if(matrix[i][0]==0||matrix[0][j]==0){
+                           matrix[i][j]=0;
+                    }
                 }
             }
+    }
+    if(matrix[0][0]==0){
+        for(int j=0;j<m;j++) matrix[0][j]=0;
+    }
+    if(col0==0){
+        for(int i=0;i<n;i++){
+            matrix[i][0]=0;
         }
     }
-   public static void main(String[] args) {
+}
+   
+public static void main(String[] args) {
     int arr[][]={{1,0,1,1},{1,0,1,1},{1,1,1,1},{1,1,1,0}};
     System.out.println("matrix:");
     for(int i=0;i<arr.length;i++){
