fd8c3705731b3e272c5e4d9ab8170e7eb2b465d2
Sagar <sagarwaghmare1384@gmail.com>
2025-03-04 08:01:23 +0530

    Introduction to Binary Tree

---
+++ Trees/IntroductionToBinaryTrees.java
@@ -0,0 +1,7 @@
+// Binary Tree
+// Full BT-Either have 0 or2 children
+// Complete binary tree-All levels are completely filled only the  last is only the left side is possibly filled fully.
+// Perfect Binary Tree-All leaf nodes are at same level.
+// Balanced BT-Height of the tree max log(n) n=8 nodes log2(8)=3
+//Degenerated Tree only one side is called as the DBT.Every node has single node.
+
