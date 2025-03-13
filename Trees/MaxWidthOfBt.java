56746c3b9f23210f4cb288525c21b27a09597191
Sagar <sagarwaghmare1384@gmail.com>
2025-03-13 19:03:12 +0530

    Max width error fixed

--- Trees/MaxWidthOfBt.java
+++ Trees/MaxWidthOfBt.java
@@ -1,61 +1,61 @@
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 
 public class MaxWidthOfBt {
-    public static class  TreeNode {
-    
+    public static class TreeNode {
         int data;
         TreeNode left;
         TreeNode right;
-        TreeNode(int data,TreeNode left,TreeNode right){
-            this.data=data;
-            this.left=null;
-            this.right=null;
-        }
-        TreeNode(int data){
-            this.data=data;
-            this.left=null;
-            this.right=null;
+        
+        TreeNode(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
         }
     }
-    public static class Pair{
+
+    public static class Pair {
         TreeNode node;
         int hd;
-        Pair(TreeNode node,int hd){
-            this.node=node;
-            this.hd=hd;
+
+        Pair(TreeNode node, int hd) {
+            this.node = node;
+            this.hd = hd;
         }
     }
-    // indexing is based on the 0 is like left 2*i+1 and right 2*i+2
-    // Formula of the width :last- first+1;
-    public static int maxWidth(TreeNode root){
-    if(root==null) return 0;
-    int ans=0;
-    Queue<Pair> q=new LinkedList<>();
-    q.offer(new Pair(root, 0));
-    while (!q.isEmpty()) {
-        int size=q.size();
-        int min=q.peek().hd;
-        int first=0;
-        int last=0;
-        for(int i=0;i<size;i++){
-            int currmin=q.peek().hd;
-            TreeNode node=q.peek().node;
-            q.poll();
-            if(i==0) first=currmin;
-            if(i==size-1) last=currmin;
-            if(node.left!=null){
-                q.offer(new Pair(node.left, currmin*2+1));
+
+    public static int maxWidth(TreeNode root) {
+        if (root == null) return 0;
+
+        int ans = 0;
+        Queue<Pair> q = new LinkedList<>();
+        q.offer(new Pair(root, 0));
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+            int min = q.peek().hd;
+            int first = 0, last = 0;
+
+            for (int i = 0; i < size; i++) {
+                Pair currentPair = q.poll(); 
+                int currmin = currentPair.hd;
+                TreeNode node = currentPair.node;
+
+                if (i == 0) first = currmin;
+                if (i == size - 1) last = currmin;
+
+                if (node.left != null) {
+                    q.offer(new Pair(node.left, currmin * 2 + 1));
+                }
+                if (node.right != null) {
+                    q.offer(new Pair(node.right, currmin * 2 + 2));
+                }
             }
-            if(node.right!=null){
-                q.offer(new Pair(node.right, currmin*2+2));
-            }
+            ans = Math.max(ans, last - first + 1);
         }
-        ans=Math.max(ans, last-first+1);
+        return ans;
     }
-    return ans;
-    }
+
     public static void main(String[] args) {
         TreeNode root = new TreeNode(1);
         root.left = new TreeNode(2);
@@ -65,6 +65,6 @@ public class MaxWidthOfBt {
         root.right.left = new TreeNode(6);
         root.right.right = new TreeNode(7);
 
-        System.out.println("Maximum Width of the binary Tree:"+maxWidth(root));
+        System.out.println("Maximum Width of the binary Tree: " + maxWidth(root));
     }
 }
