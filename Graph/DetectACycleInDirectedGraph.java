38f6ac817c6098b052644dd26437e3864a3b7512
Sagar <sagarwaghmare1384@gmail.com>
2025-04-17 20:09:22 +0530

    Implement cycle detection in directed graphs using DFS

---
+++ Graph/DetectACycleInDirectedGraph.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class DetectACycleInDirectedGraph {
+    // Using the dfs techinque detect cycle in directed Graph
+    public static boolean dfsCheck(int node,ArrayList<ArrayList<Integer>>adj,int vis[],int pathVis[]){
+        vis[node]=1;
+        pathVis[node]=1;
+        for (int it : adj.get(node)) {
+            if(vis[it]==0){
+                if(dfsCheck(it, adj, vis, pathVis)==true){
+                    return true;
+                }
+            }
+            else if(pathVis[it]==1){
+                return true;
+            }
+            
+        }
+        pathVis[node]=0;
+        return false;
+
+    }
+    public static boolean isCycle(int v,ArrayList<ArrayList<Integer>> adj){
+        int vis[]=new int[v];
+        int pathVis[]=new int[v];
+        for (int i = 0; i < v; i++) {
+            if(vis[i]==0){
+              if(dfsCheck(i,adj,vis,pathVis)==true){
+                return true;
+              }
+            }
+        }
+        return false;
+    }
+   public static void main(String[] args) {
+        int v = 4;
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+        for (int i = 0; i < v; i++) {
+            adj.add(new ArrayList<>());
+        }
+        adj.get(0).add(1);
+        adj.get(1).add(2);
+        adj.get(2).add(3);
+        adj.get(3).add(0);
+
+        boolean ans = isCycle(v, adj);
+        if (ans) {
+            System.out.println("Cycle Detected");
+        } else {
+            System.out.println("No Cycle Detected");
+        }
+    }
+}
