a76b61df0cf0d18a6049e05daa2693fe78c86f7a
Sagar <sagarwaghmare1384@gmail.com>
2025-04-18 09:32:44 +0530

    Refactor DFS cycle detection method for improved readability and consistency

--- Graph/DetectACycleInDirectedGraph.java
+++ Graph/DetectACycleInDirectedGraph.java
@@ -2,37 +2,36 @@ import java.util.ArrayList;
 
 public class DetectACycleInDirectedGraph {
     // Using the dfs techinque detect cycle in directed Graph
-    public static boolean dfsCheck(int node,ArrayList<ArrayList<Integer>>adj,int vis[],int pathVis[]){
-        vis[node]=1;
-        pathVis[node]=1;
+    public static boolean dfsCheck(int node, ArrayList<ArrayList<Integer>> adj, int vis[], int pathVis[]) {
+        vis[node] = 1;
+        pathVis[node] = 1;
         for (int it : adj.get(node)) {
-            if(vis[it]==0){
-                if(dfsCheck(it, adj, vis, pathVis)==true){
+            if (vis[it] == 0) {
+                if (dfsCheck(it, adj, vis, pathVis) == true) {
+                    return true;
+                }
+            } else if (pathVis[it] == 1) {
+                return true;
+            }
+        }
+        pathVis[node] = 0; // Set pathVis[node] back to 0 after fully exploring the node and its descendants
+        return false;
+    }
+
+    public static boolean isCycle(int v, ArrayList<ArrayList<Integer>> adj) {
+        int vis[] = new int[v];
+        int pathVis[] = new int[v];
+        for (int i = 0; i < v; i++) {
+            if (vis[i] == 0) {
+                if (dfsCheck(i, adj, vis, pathVis) == true) {
                     return true;
                 }
             }
-            else if(pathVis[it]==1){
-                return true;
-            }
-            
         }
-        pathVis[node]=0;
         return false;
+    }
 
-    }
-    public static boolean isCycle(int v,ArrayList<ArrayList<Integer>> adj){
-        int vis[]=new int[v];
-        int pathVis[]=new int[v];
-        for (int i = 0; i < v; i++) {
-            if(vis[i]==0){
-              if(dfsCheck(i,adj,vis,pathVis)==true){
-                return true;
-              }
-            }
-        }
-        return false;
-    }
-   public static void main(String[] args) {
+    public static void main(String[] args) {
         int v = 4;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for (int i = 0; i < v; i++) {
