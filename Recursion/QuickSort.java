a5e245225c16296f4b6938f6a05e156e57deee23
Sagar <sagarwaghmare1384@gmail.com>
2025-06-13 20:39:21 +0530

    Quick Sort

--- Recursion/QuickSort.java
+++ Recursion/QuickSort.java
@@ -1,9 +1,36 @@
 public class QuickSort{
     public static void printList(int[]arr){
         for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+            System.out.print(arr[i]+" ");
         }
         System.out.println();
+    }
+
+    public static int partition(int[]arr,int low,int high){
+        // Getting the pivot element
+        int pivot=arr[high];
+        // Index of smaller element
+        int i=low-1;
+        for(int j=low;j<high;j++){
+            if(arr[j]<pivot){
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+
+        // Swap arr[i+1] and arr[high] (or pivot)
+
+        int temp=arr[i+1];
+        arr[i+1]=arr[high];
+        arr[high]=temp;
+        return i+1;                                  
+
+
+
+
     }
     public static void QuickSort(int[]arr,int low,int high){
         // 1 st element in the array
@@ -13,6 +40,13 @@ public class QuickSort{
 
         //pivot & please it in its correct please int the sorted array.
         
+        if(low<high){
+            int pivot=partition(arr,low,high);
+            QuickSort(arr, low, pivot - 1); 
+            QuickSort(arr, pivot + 1, high);
+
+        }
+
     }
     public static void main(String[] args) {
        int arr[]={4,6,2,5,7,9,1,3};
